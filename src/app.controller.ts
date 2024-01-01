be2532e9f8b090e1d4fea92f396ac839b54a0a63
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-01-01 19:33:44 +0630

    about.hbs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Render, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { title } from 'process';
 
 @Controller()
 export class AppController {
@@ -7,6 +8,24 @@ export class AppController {
   @Get()
   root(@Res() res: Response) {
     return res.render('index', { message: 'Hello world!!' });
+  }
+
+  @Get("/about")
+  @Render("about")
+  about(){
+
+      let viewData = [];
+      viewData["description"] = "This is about page ...";
+      viewData["author"] = "Developed By Hein Wai Yan Htet";
+      let data1 = "About us - Online Store";
+
+      return{
+        title : data1,
+        subtitle : "About us",
+        viewData : viewData
+      };
+
+
   }
 
 
