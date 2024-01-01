6a6b671403e99a57f763f7ddea6df54870b8bf06
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-01-01 21:37:50 +0630

    products added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import path from 'path';
+import { ProductsController } from './products.controller';
 
 @Module({
   imports: [
   ],
-  controllers: [AppController],
+  controllers: [AppController,ProductsController],
 })
 export class AppModule {}
--- src/products.controller.ts
+++ src/products.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Param, Render } from '@nestjs/common';
+
 @Controller('/products')
-export class ProductsController {
+export class ProductsController{
+    
     static products = [
         {
             id: '1',
@@ -30,16 +32,36 @@ export class ProductsController {
             image: 'game.png',
             price: '100',
         },
-    ];
+    ]
+
     @Get('/')
     @Render('products/index')
-    index() {
+    index(){
+
         const viewData = [];
         viewData['title'] = 'Products - Online Store';
         viewData['subtitle'] = 'List of products';
         viewData['products'] = ProductsController.products;
+
         return {
-            viewData: viewData,
-        };
+            viewData : viewData,
+        }
     }
+
+
+    @Get('/:id')
+    @Render('products/show')
+    show(@Param() params)
+    {
+        const product = ProductsController.products[params.id -1];
+        const viewData = [];
+        viewData['title'] = product.name + '-Online Store';
+        viewData['subtitle'] = product.name + '-Product Information';
+        viewData['product'] = product;
+        return{
+            viewData : viewData,
+        }
+    }
+
+
 }
