e587e64be89d0d6c14ba517b378bb8456a162803
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-01-02 02:14:39 +0630

    driver not found error solve

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,18 @@ import { ProductsController } from './products.controller';
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(),
+  imports: 
+  [
+    TypeOrmModule.forRoot({
+      "type": "mysql",
+      "host": "localhost",
+      "port": 3306,
+      "username": "test",
+      "password": "password",
+      "database": "test",
+      "entities": ["dist/**/*.entity{.ts,.js}"],
+      "synchronize": true
+    }),
   ],
   controllers: [AppController,ProductsController],
 })
