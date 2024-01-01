3fad88ea6d2c8f3e491bc3031af9be0e2c990cd4
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-01-01 22:58:51 +0630

    database credentails

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import path from 'path';
 import { ProductsController } from './products.controller';
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot(),
   ],
   controllers: [AppController,ProductsController],
 })
